8676651308a7960dd8a11df16c87427db2f74eec
Jack Shi <shisoftgenius@gmail.com>
2013-09-10 04:37:39 +0800

    Entrance

--- wiki-dump-to-mongo/src/main/java/com/shisoft/wikiDumpToMongo/App.java
+++ wiki-dump-to-mongo/src/main/java/com/shisoft/wikiDumpToMongo/App.java
@@ -1,13 +1,22 @@
 package com.shisoft.wikiDumpToMongo;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.util.Scanner;
+
+public class App
 {
+
+    static String database;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Wiki-Dump-To-Mongo. Import Wikipedia Dump Files to MongoDB. Assist Module for WebFusion MK II");
+        System.out.println("(C) 2013 Shisoft Corporation");
+        System.out.println("You Need To Put Uncompressed Data Files In Home Path To Continue. We Will Search And Import Automatically.");
+        System.out.print("Tell me where is the database:");
+        database = sc.next();
+        System.out.println("Searching for XML and SQL files...");
+
     }
 }
